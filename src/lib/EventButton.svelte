709947fe6aa74c757090e4833792e454902e34c7
Christian Gabrielsson <chrisgabrielsson@protonmail.com>
2025-04-30 18:41:07 -0700

    added mobile first layout for search and event list, looks pretty sick

---
+++ src/lib/EventButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" >
+    let { edmEvent }  = $props();
+</script>
+
+<a class="backdrop-blur-xl bg-gradient-to-r from-cyan-500/80 to-purple-600/80 border
+            border-white/20 px-4 py-2 rounded-full  font-bold text-base transition-all
+            duration-300 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)] hover:border-white/40"
+   href="{edmEvent.TicketUrl}" target="_blank">View Event</a>
---
+++ src/lib/EventDate.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import {Months, Weekdays} from "$lib/Types";
+    let { edmEvent }  = $props();
+
+    function getFormattedMonth(wholeDateStringUTC: Date) {
+        const currentMonth = new Date(wholeDateStringUTC).getUTCMonth()
+        return Months.find((month, index) => {
+            return (index === currentMonth) && month
+        })
+    }
+
+    function getFormattedDay(wholeDateStringUTC: Date) {
+        return new Date(wholeDateStringUTC).getUTCDate();
+    }
+
+    function getFormattedWeekday(wholeDateStringUTC: Date) {
+        const currentWeekday = new Date(wholeDateStringUTC).getUTCDay()
+        return Weekdays.find((weekday, index) => {
+            return (index === currentWeekday) && weekday
+        })
+    }
+
+    function getFormattedYear(wholeDateStringUTC: Date) {
+        return new Date(wholeDateStringUTC).getUTCFullYear();
+    }
+
+
+</script>
+
+<div class="flex flex-col ">
+    <div class="flex font-bold sm:font-extrabold text-2xl sm:text-3xl">
+        <div class="mr-3">{getFormattedMonth(edmEvent.EventDate)}</div>
+        <div class="">{getFormattedDay(edmEvent.EventDate)}</div>
+    </div>
+    <div class="flex items-baseline">
+        <div class="font-medium mr-3">{getFormattedWeekday(edmEvent.EventDate)}</div>
+        <div class="text-sm">{getFormattedYear(edmEvent.EventDate)}</div>
+    </div>
+</div>
