c051fa7cbc58a332adcf073d8f409cc2216935ea
Christian Gabrielsson <chrisgabrielsson@protonmail.com>
2025-04-30 20:21:06 -0700

    added menu, also added proper artist filtering for accents

--- src/lib/EventTable.svelte
+++ src/lib/EventTable.svelte
@@ -4,13 +4,19 @@
     import {EdmEventInputId, type EdmEventInterface} from "$lib/Types";
 
     let { data } = $props();
+    const filterTextAccents = (searchQuery: string) => searchQuery.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+    function filterArtistNames(artistName: string, searchQuery: string): Boolean {
+        artistName = filterTextAccents(artistName)
+        return artistName.includes(searchQuery)
+    }
 
     function filterEvents(edmEvents: [EdmEventInterface], searchQuery: string) {
         let filteredEdmEventsData: EdmEventInterface[] = [];
 
         switch(getSelectedCategory()) {
             case EdmEventInputId.artistName:
-                filteredEdmEventsData = edmEvents.filter((edmEvent: EdmEventInterface) => edmEvent.ArtistName.includes(searchQuery))
+                filteredEdmEventsData = edmEvents.filter((edmEvent: EdmEventInterface) => filterArtistNames(edmEvent.ArtistName,searchQuery))
             break;
 
             case EdmEventInputId.venue:
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <div class="bg-gradient-to-br h-[65%] w-full from-purple-900 via-indigo-900 to-black py-10 ">
-	<div class="relative  h-[100%] w-7xl mx-auto rounded-xl">
+	<div class="relative  h-[100%] md:w-7xl mx-auto rounded-xl">
 		<div class="absolute -inset-6">
 			<div class="absolute top-1/4 left-1/4 w-64 h-64 bg-cyan-400 rounded-full blur-xl opacity-50"></div>
 			<div class="absolute bottom-1/4 right-1/4 w-80 h-80 bg-pink-400 rounded-full blur-xl opacity-50"></div>
 			<div class="absolute top-3/4 left-1/3 w-48 h-48 bg-purple-400 rounded-full blur-xl opacity-50"></div>
 		</div>
 
-		<div class=" overflow-y-auto overscroll-contain backdrop-blur-xl h-[100%] bg-white/15 border border-white/30 rounded-xl z-10 p-5 mt-2 ">
+		<div class="overflow-y-auto overscroll-contain backdrop-blur-xl h-[100%] bg-white/15 border border-white/30 rounded-xl z-10 p-5 mt-2 ">
 
 			<div class="shadow-2xl space-y-8 rounded-xl p-4 h-full w-full overflow-y-auto
             flex flex-col text-slate-300 font-semibold text-lg tracking-wider">
